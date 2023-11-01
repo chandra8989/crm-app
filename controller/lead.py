80cc6e4208d02f87f5ecb356b8b3cde7b2719868
chandra8989 <147043309+chandra8989@users.noreply.github.com>
2023-11-01 09:08:44 +0000

    Lead feature (#1)
    *Full load feature  done and tested

--- controller/lead.py
+++ controller/lead.py
@@ -1,1 +1,14 @@
 lead.py
+# Manage lead controller implementation
+
+#Create lead
+def create_lead();
+  print "Lead creation done"
+
+  #update lead
+  def update_lead();
+     print " lead update done"
+
+  # get lead
+  def get_lead()
+     print "get lead done"
--- model/lead.py
+++ model/lead.py
@@ -1,1 +1,14 @@
 lead.py
+# Lead Model imlplemetation
+
+# Insert lead query
+def insert_lead();
+  print "Lead inserted into database"
+
+  #update lead query
+  def update_lead_query();
+    print "lead updated into database"
+
+    #get lead query
+    def select_lead_query();
+      print "lead select from database"
